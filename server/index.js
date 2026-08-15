191167f42a7999033e5e7d3661d928453f7985b9
Mahesh Kakde <maheshkakde100@gmail.com>
2026-08-15 23:49:28 +0530

    Remove extra newline in index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-
 app.use(cors());
 app.use(router);
 io.on('connect', (socket) => {
